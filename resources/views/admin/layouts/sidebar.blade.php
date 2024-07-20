970f8fc55a444245ea4ecb2fc051717bf535c0a6
satinderpanesar03 <satinderpanesar03@gmail.com>
2024-07-20 17:05:43 +0530

    Signed-off-by: satinderpanesar03 <satinderpanesar03@gmail.com>

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -144,8 +144,8 @@
                             </li>
                             {{-- <li class="nav-item {{ request()->routeIs('admin.purchase.purchase.follow-up') ? 'active' : '' }}""><a href=" {{route('admin.purchase.purchase.follow-up')}}"><i class="ft-arrow-right submenu-icon"></i><span class="menu-item " data-i18n="roles">Follow Up</span></a>
                             </li> --}}
-                            <li class="nav-item {{ request()->routeIs('admin.purchase.purchase.orders') ? 'active' : '' }}""><a href=" {{route('admin.purchase.purchase.orders')}}"><i class="ft-arrow-right submenu-icon"></i><span class="menu-item" data-i18n="permissions">Orders</span></a>
-                            </li>
+                            {{-- <li class="nav-item {{ request()->routeIs('admin.purchase.purchase.orders') ? 'active' : '' }}""><a href=" {{route('admin.purchase.purchase.orders')}}"><i class="ft-arrow-right submenu-icon"></i><span class="menu-item" data-i18n="permissions">Orders</span></a>
+                            </li> --}}
                         </ul>
                     </li>
                     @endif
